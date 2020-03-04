ccad9d46059c8bbebdbd0815c0dc82e502fa5577
Lambda_School_Loaner_198 <lambda_school_loaner_198@Lambda-School-Loaner-198s-MacBook-Pro.local>
2020-03-04 09:46:06 -0800

    updated docs

--- docs/api_project.js
+++ docs/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-03-04T00:09:08.407Z",
+    "time": "2020-03-04T17:45:53.504Z",
     "url": "http://apidocjs.com",
     "version": "0.20.0"
   }
